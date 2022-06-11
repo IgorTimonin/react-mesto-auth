e430e15bf50efe5fe060708598b7dc0d6039dd9d
IgorTimonin <mut4nt@yandex.ru>
2022-06-11 17:30:48 +0300

    checklist remarked

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App.js';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
