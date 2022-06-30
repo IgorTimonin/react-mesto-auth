c21c34cd898b960f4fd6712ee00307d60730287b
IgorTimonin <mut4nt@yandex.ru>
2022-06-30 14:17:34 +0300

    user block in header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 export default function Header() {
   return (
     <header className='header'>
-      <a className='header__logo' href='#'/>
+      <a className='header__logo' href='#' />
+      <div className='header__user-block'>
+        <div className='header__user-email'>useremail@email.com</div>
+        <button className='header__user-btn'>Выход</button>
+      </div>
     </header>
   );
 }
